8841e66dd480060869010e57e88296bb92ea6628
Amabeli_Nicco <Amabeli_Nicco@G-02-03>
2021-09-02 09:25:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWrld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there");
+            //Asks user age.
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            //Gives user a place to input age
+            Console.WriteLine($"You are {input} years old!");
+            Console.WriteLine("General Kenobi");
+        }
+    }
+}
